d53fc48679d57661c8ae3c50b36ca2762fc3c8b4
Tong Liu <402169950@qq.com>
2022-11-14 20:09:48 -0500

    Added more unit tests.

--- SSG/tests/ssgTests.py
+++ SSG/tests/ssgTests.py
@@ -2,6 +2,7 @@ import unittest
 
 from SSG.utils.input import parseInput
 import os
+from os.path import exists
 import shutil
 import warnings
 
@@ -25,13 +26,13 @@ class SSGTest(unittest.TestCase):
         parseInput(arg)
         site = os.path.join(os.path.abspath(os.getcwd()), "dist", "Test Title.html")
 
-        succsess = False
+        success = False
         with open(site, "r", encoding="utf-8") as file:
             lines = file.read().splitlines()
 
             for line in lines:
                 if line == "<p>Test Body</p>":
-                    succsess = True
+                    success = True
                     break
 
     # Tests Parse Input for body text
@@ -40,16 +41,30 @@ class SSGTest(unittest.TestCase):
         parseInput(arg)
         site = os.path.join(os.path.abspath(os.getcwd()), "dist", "Test Title.html")
 
-        succsess = False
+        success = False
         with open(site, "r", encoding="utf-8") as file:
             lines = file.read().splitlines()
 
             for line in lines:
                 if line.find("<em>Body</em>"):
-                    succsess = True
+                    success = True
                     break
 
-        self.assertTrue(succsess)
+        self.assertTrue(success)
+
+    # Tests index creation
+    def test_indexCreation(self):
+        arg = os.path.join(os.path.abspath(os.getcwd()), "testFiles")
+        parseInput(arg)
+        indexFile = os.path.join(os.path.abspath(os.getcwd()), "dist", "index.html")
+        success = False
+
+        if exists(indexFile):
+            with open(indexFile, "r", encoding="utf-8") as file:
+                indexStr = file.read()
+                success = indexFile != ""
+
+        self.assertTrue(success)
 
     def tearDown(self):
         warnings.simplefilter("default", ResourceWarning)
